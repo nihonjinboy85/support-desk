44032f8775a7ed7e4fdca78e8ffcc883c589bd2a
Richard Carrigan <v-rcarrigan@microsoft.com>
2022-07-15 13:41:25 -0700

    Create user routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the support desk API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
