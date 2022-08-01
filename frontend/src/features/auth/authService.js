b1627501d4828103f9f2ec468e1af1b0576e9f26
Richard Carrigan <v-rcarrigan@microsoft.com>
2022-08-01 11:44:00 -0700

    Add return statement to fix navbar links not changing on login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -23,6 +23,8 @@ const login = async userData => {
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
+
+  return response.data;
 };
 
 const authService = {
